257875df2480757d25fb2af9ec424e786c0a07ce
Vu Trong Hieu <tset0401@gmail.com>
2016-10-03 15:30:22 +0000

    release timestamp app

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static('public'));
+app.get('/:userdate',function(req,res){
+        var date = req.params.userdate;
+        var unix = null;
+        var natural = null;
+        function toUnix(date) {
+            return moment(date, "MMMM D, YYYY").format("X");
+        }
+        
+        function toNatural(unix) {
+            return moment.unix(unix).format("MMMM D, YYYY");
+        }
+    
+        if (+date >= 0) {
+            unix = +date;
+            natural = toNatural(unix);
+        } 
+    
+        if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
+            unix = +toUnix(date);
+            natural = toNatural(unix);
+        }
+        
+        var dateObj = { "unix": unix, "natural": natural };
+        res.end(JSON.stringify(dateObj));
+        
+    
 });
-
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
